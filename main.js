b9253ce9bfe50556a696e2c2ef4b6794226329ac
felix <felixmetge25@gmail.com>
2025-01-30 15:19:59 +0100

    ajout du javascript a la partie histoire de réussite

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// faire un don
 const euro = document.querySelectorAll(".containerDon div div");
 const boutons = document.querySelectorAll('#button > button');
 for(const i in euro){
@@ -33,5 +35,33 @@ for(const i in boutons){
             a = "/ans";
         }
         document.getElementById('periode').innerHTML= " "+a;
+    }
+}
+
+// histoire de réussite changer photo
+
+const photo = document.querySelectorAll('.grid > div');
+for(let i = 1 ; i<=3 ;i++){
+    photo[i].onclick = function (){
+        document.querySelector('.animaux').style.backgroundImage = window.getComputedStyle(photo[i]).backgroundImage;
+        photo[1].style.border = "none";
+        photo[2].style.border = "none";
+        photo[3].style.border = "none";
+        photo[i].style.border = "solid 7px #3AA331";
+        photo[i].style.borderRadius = "25px";
+        if(i=== 1){
+            document.getElementById('nom').innerHTML="Luna";
+        document.getElementById('details').innerHTML="Trouvée dans la rue avec une patte cassée, Luna a été soignée et  a retrouvé sa joie de vivre. elle coule maintenant des jours heureux dans sa nouvelle famille";
+        document.getElementById('light').innerHTML="Janvier 2024";
+        }else if (i === 2){
+            document.getElementById('nom').innerHTML="Oscar";
+            document.getElementById('details').innerHTML="Abandonné dans une forêt, Oscar était terrifié par les humains. Après des mois de patience et d’amour, il est devenu un chat confiant et affectueux.";
+            document.getElementById('light').innerHTML="Mars 2024";
+        }else if (i === 3){
+            document.getElementById('nom').innerHTML="Max";
+            document.getElementById('details').innerHTML="Sauvé d'un laboratoire, Max n'avait jamais connu l'amour. Aujourd'hui, il profite de sa liberté et fait le bonheur de sa famille d'accueil.";
+            document.getElementById('light').innerHTML="Février 2024";
+        }
+
     }
 }
