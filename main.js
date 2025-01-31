9bd86749d296f732047160b4585ca9babd393b19
felix <felixmetge25@gmail.com>
2025-01-31 13:24:13 +0100

    ajout du javascript a la partie contact

--- main.js
+++ main.js
@@ -118,33 +118,3 @@ function vrai(elementGauche, elementdroite) {
 
 }
 
-//contact-form
-function formulaire() {
-const nom = document.getElementById('name');
-const email = document.getElementById('email');
-const sujet = document.getElementById('sujet');
-const msg = document.getElementById('msg');
-const err = document.querySelectorAll('.error');
-    let isTrue = true
-    for(const i of err){
-        i.innerHTML= "";
-    }
-    if(nom.value === ""){
-        isTrue = false
-        err[0].innerHTML="entrez votre nom"
-    }
-    if(!email.value.include("@")){
-        isTrue = false
-        err[1].innerHTML="entrez un email valid"
-    }
-    if(sujet.value === ""){
-        isTrue=false
-        err[2].innerHTML="choisissez un sujet"
-    }
-    if(msg.value === ""){
-        isTrue = false
-        err[3].innerHTML="entrez votre message"
-    }
-    console.log(isTrue)
-    return isTrue;
-}
