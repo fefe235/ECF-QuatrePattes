dc9987960d5c3ce2f8cb18efcf20f689ee3031df
felix <felixmetge25@gmail.com>
2025-01-31 17:00:02 +0100

    ajout du menu burger

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+//menu burger
+const menu = document.getElementById('cacher')
+menu.onclick = function (){
+    menu.style.display = "none"
+    document.getElementById('header').style.height="30vh"
+    document.getElementById('nav').innerHTML = " <img src='images/xW.png' id ='x'><a href='index.html'>Acceuil</a> <a href='contact.html'>Contact</a> <a href='#encre' id='donner'><button><img src='images/heartW 2.png' alt='petit coeur'>Faire un don</button></a>";
+    document.getElementById('nav').style.display = "flex";
+    document.getElementById('header').style.display = "flex";
+    document.getElementById('header').style.flexDirection = "column";
+    document.getElementById('nav').style.flexDirection = "column";
+    document.getElementById('donner').style.display= "block"
+    document.getElementById('x').onclick= function(){
+        
+        document.getElementById('nav').innerHTML = "";
+        menu.style.display="flex";
+    document.getElementById('header').style.height="8vh"
+    document.getElementById('header').style.flexDirection = "row";
+    document.getElementById('nav').style.flexDirection = "row";
 
+    }
+
+}
 // faire un don
 const euro = document.querySelectorAll(".containerDon div div");
 const boutons = document.querySelectorAll('#button > button');
