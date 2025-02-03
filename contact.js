40512d7ddbd4e80b9b83de34509c5cf6fbee7582
felix <felixmetge25@gmail.com>
2025-02-03 14:59:15 +0100

    modif de ciblage pour le formulaire

--- contact.js
+++ contact.js
@@ -32,26 +32,30 @@ function formulaire() {
     const email = document.getElementById('email');
     const sujet = document.getElementById('sujet');
     const msg = document.getElementById('msg');
-    const err = document.querySelectorAll('.error');
+    const errA = document.getElementById('errorA');
+    const errB = document.getElementById('errorB');
+    const errC = document.getElementById('errorC');
+    const errD = document.getElementById('errorD');
         let isTrue = true
-        for(const i of err){
-            i.innerHTML= "";
-        }
+        errA.innerHTML ="";
+        errB.innerHTML ="";
+        errC.innerHTML ="";
+        errD.innerHTML ="";
         if(nom.value === ""){
             isTrue = false
-            err[0].innerHTML="entrez votre nom"
+            errA.innerHTML="entrez votre nom"
         }
         if(!email.value.includes("@")){
             isTrue = false
-            err[1].innerHTML="entrez un email valid"
+            errB.innerHTML="entrez un email valid"
         }
         if(sujet.value === ""){
             isTrue=false
-            err[2].innerHTML="choisissez un sujet"
+            errC.innerHTML="choisissez un sujet"
         }
         if(msg.value === ""){
             isTrue = false
-            err[3].innerHTML="entrez votre message"
+            errD.innerHTML="entrez votre message"
         }
         console.log(isTrue)
         return isTrue;
